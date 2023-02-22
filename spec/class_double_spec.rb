cab3f39d3164c97f2c2c672b75c03cdb48339b21
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2023-02-22 16:50:23 -0300

    Finish section 4 Mocks

--- spec/class_double_spec.rb
+++ spec/class_double_spec.rb
@@ -14,10 +14,10 @@ end
 
 RSpec.describe CardGame do
   it 'can only implement class methods that are defined on a class' do
-    deck_klass = class_double(Deck, build: ['Ace', 'Queen']).as_stubbed_const
+    deck_klass = class_double(Deck, build: %w[Ace Queen]).as_stubbed_const
 
     expect(deck_klass).to receive(:build)
     subject.start
-    expect(subject.cards).to eq(['Ace', 'Queen'])
+    expect(subject.cards).to eq(%w[Ace Queen])
   end
 end
